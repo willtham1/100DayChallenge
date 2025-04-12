346e021c77fc4f05a5c0c64b3de6b3255174eb59
Will Tham <tham.will00@gmail.com>
2025-04-11 23:46:25 -0700

    Initial Commit

---
+++ Day1/main.py
@@ -0,0 +1,7 @@
+print("Will Tham")
+print("04/11/2025")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+print("I am feeling confident that I can complete this challenge. 😭")
+print("You can follow my progress at replit.com/@willTham1")
