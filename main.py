b88b3fe9a61aabb1eca7dbd94843f2546fd129b5
Will Tham <tham.will00@gmail.com>
2025-04-22 23:27:41 -0700

    Day20

---
+++ main.py
@@ -0,0 +1,9 @@
+starting_numbner = int(input("Start at: "))
+ending_number = int(input("End before: "))
+increment = int(input("Increment berween values(this can be a negative): "))
+
+for i in range(starting_numbner, ending_number, increment):
+    print(i)
+
+
+# Day 20 we learned more indept about the range built in funciton and how to use it in a for loop.
